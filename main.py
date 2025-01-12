e0d4788a2cb77d5c21f45e21623f9df9bb7f0e51
Miko <mikola.syabila@gmail.com>
2025-01-12 21:05:33 +0700

    ngebuat print important thing

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ if __name__ == "__main__":
     print("this is multiplication of num1 and num2",num1*num2)
     print("This is the main module")
     print("the square of num1 is",num1*num1)
+    print("important thing")
